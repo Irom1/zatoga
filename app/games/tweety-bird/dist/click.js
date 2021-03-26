632dd6ade218524b6c4a91778ec89ff9db77a966
Yo Yo <morifinlayson@gmail.com>
2021-03-26 13:34:55 +0000

    Update to add tweety bird

--- app/games/tweety-bird/dist/click.js
+++ app/games/tweety-bird/dist/click.js
@@ -1,6 +1,15 @@
-var canvas = document.getElementById('bird-game');
+//var canvas = document.getElementById('bird-game');
 function clicker(e) {
   if (e.key === ' ' || e.key === 'Spacebar') {
-    alert("You need to tap to play - clicking space doesn't work.");
+    alert("You need to tap/click the screen to play - clicking space doesn't work.");
   }
+}
+
+// Force app size
+var inApp = window.matchMedia('(display-mode: standalone)').matches || window.navigator.standalone === true;
+if(inApp) {
+  window.resizeTo(480, 640);
+  window.addEventListener('resize', () => {
+    window.resizeTo(480, 640);
+  });
 }
